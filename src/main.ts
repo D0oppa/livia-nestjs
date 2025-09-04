900aace6ce7f5a2887ce244cc290bc63b2ab9d23
RoberthLoorDev <dayvidlife@gmail.com>
2025-09-03 22:21:06 -0500

    ADD: class-transformer and ValidationPipe global configuration in main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // class-validator global configuration
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //delete extra fields
+      forbidNonWhitelisted: true, //throw error if extra fields are sent
+      transform: true, //transform types (e.g., string → number)
+      transformOptions: {
+        enableImplicitConversion: true, //automatically convert basic types
+      },
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
