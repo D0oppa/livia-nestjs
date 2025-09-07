03e08ffbdf710dd056b5b9d0700b9caa928affaf
RoberthLoorDev <dayvidlife@gmail.com>
2025-09-07 06:52:14 -0500

    ADD: Import DoctorsModule and PrismaModule in AppModule; format PrismaModule and PrismaService for consistency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { DoctorsModule } from './doctors/doctors.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [DoctorsModule, PrismaModule],
   controllers: [],
   providers: [],
 })
